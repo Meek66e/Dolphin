d6a72daffc43afb7ec6c1a3d5940e328da53a682
Meek66e <paul.meek9@gmail.com>
2015-01-21 14:54:24 -0500

    Updated file extensions

--- Mantaray/ultralcd.cpp
+++ Mantaray/ultralcd.cpp
@@ -1,7 +1,7 @@
 #include "temperature.h"
 #include "ultralcd.h"
 #ifdef ULTRA_LCD
-#include "Marlin.h"
+#include "Mantaray.h"
 #include "language.h"
 #include "cardreader.h"
 #include "temperature.h"
@@ -1117,7 +1117,7 @@ void lcd_update()
         u8g.firstPage();
         do
         {
-            u8g.setFont(u8g_font_6x10_marlin);
+            u8g.setFont(u8g_font_6x10_mantaray);
             u8g.setPrintPos(125,0);
             if (blink % 2) u8g.setColorIndex(1); else u8g.setColorIndex(0); // Set color for the alive dot
             u8g.drawPixel(127,63); // draw alive dot
