52d826a10684a2cd8ea7110188503133a7e75c6e
Meek66e <paul.meek9@gmail.com>
2015-01-21 14:49:28 -0500

    Updated file extensions

--- Mantaray/stepper.cpp
+++ Mantaray/stepper.cpp
@@ -21,7 +21,7 @@
 /* The timer calculations of this module informed by the 'RepRap cartesian firmware' by Zack Smith
    and Philipp Tiefenbacher. */
 
-#include "Marlin.h"
+#include "Mantaray.h"
 #include "stepper.h"
 #include "planner.h"
 #include "temperature.h"
