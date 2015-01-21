722b0bfcd562f30c33977f9ed201dcd8372fee75
Meek66e <paul.meek9@gmail.com>
2015-01-21 14:56:21 -0500

    Updated file extensions

--- Mantaray/vector_3.cpp
+++ Mantaray/vector_3.cpp
@@ -17,7 +17,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 #include <math.h>
-#include "Marlin.h"
+#include "Mantaray.h"
 
 #ifdef ENABLE_AUTO_BED_LEVELING
 #include "vector_3.h"
