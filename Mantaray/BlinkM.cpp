5c7762b35dd06ddaf454d0de99b0dab10c6e0055
Meek66e <paul.meek9@gmail.com>
2015-01-20 21:05:36 -0700

    Update BlinkM.cpp

--- Mantaray/BlinkM.cpp
+++ Mantaray/BlinkM.cpp
@@ -2,7 +2,7 @@
   BlinkM.cpp - Library for controlling a BlinkM over i2c
   Created by Tim Koster, August 21 2013.
 */
-#include "Marlin.h"
+#include "Mantaray.h"
 #ifdef BLINKM
 
 #if (ARDUINO >= 100)
