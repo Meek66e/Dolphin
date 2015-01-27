9c4732b10cf6f5ccfde2862029520bf57a88a20d
Paul Meek <paul.meek9@gmail.com>
2015-01-27 10:36:58 -0500

    Update SdFatUtil.cpp

--- Dolphin/SdFatUtil.cpp
+++ Dolphin/SdFatUtil.cpp
@@ -17,7 +17,7 @@
  * along with the Arduino SdFat Library.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
-#include "Mantaray.h"
+#include "Dolphin.h"
 
 #ifdef SDSUPPORT
 #include "SdFatUtil.h"
